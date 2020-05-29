171658e256e2b63b66d922c271a9d834f7fb1f53
abasse-lab <62431256+abasse-lab@users.noreply.github.com>
2020-05-29 13:34:52 +0100

    bank_072.py

---
+++ bank_072.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class BankAccount(object):
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print('Insufficient funds available')
+        else:
+            self.balance -= amount
+
+    def __str__(self):
+        return 'Your current balance is: {:.2f} euro'.format(self.balance)

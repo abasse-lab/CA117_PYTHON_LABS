8d462f09bb70496be42f61541b87178b230ba27f
abasse-lab <62431256+abasse-lab@users.noreply.github.com>
2020-05-29 23:31:50 +0100

    fibonacci_102.py

---
+++ fibonacci_102.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def fibonacci(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+def main():
+    print(fibonacci(0))
+    print(fibonacci(1))
+    print(fibonacci(5))
+    print(fibonacci(8))
+
+if __name__ == '__main__':
+    main()
